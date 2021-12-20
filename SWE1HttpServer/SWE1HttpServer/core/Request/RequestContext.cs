18fef57617a4de6af9c8f35274bf4bca21fffc4e
Sebastian2123 <sebastian_schramm@hotmail.com>
2021-12-20 17:57:50 +0100

    Added Constructor for RequestContext

--- SWE1HttpServer/SWE1HttpServer/core/Request/RequestContext.cs
+++ SWE1HttpServer/SWE1HttpServer/core/Request/RequestContext.cs
@@ -1,10 +1,10 @@
 using SWE1HttpServer.app.DAL;
 using SWE1HttpServer.app.Models;
 using SWE1HttpServer.core.Client;
+using SWE1HttpServer.core.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,10 +13,19 @@ namespace SWE1HttpServer.core.Request
 
     public class RequestContext 
     {
-       public HttpMethod method {get; set; }
+       public HttpMethod Method {get; set; }
        public string ResourcePath {get; set; }
        public string HttpVersion {get; set; }
-       public Dictionary<string, string> Headers {get; set; }
+       public Dictionary<string, string> Header {get; set; }
        public string Payload {get; set; }
+    
+    public RequestContext(){
+        Method = HttpMethod.Get;
+        ResourcePath="";
+        HttpVersion = "HTTP/1.1";
+        Header = new Dictionary<string, string>();
+        Payload = null;
+
+    }
     }
 }
